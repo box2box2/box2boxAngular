925fecc7a260b351a93b1df5aaaacca725127105
Erwin <erwin.torrenga@live.nl>
2025-09-03 18:36:38 +0200

    Auto-generated commit

---
+++ chunk-YCVTXF77.js
@@ -0,0 +1,1 @@
+import{$a as a,ab as b,bb as c,cb as d,db as e,eb as f}from"./chunk-ISUQLHKG.js";import"./chunk-TSRGIXR5.js";export{f as default,e as pan,d as resetZoom,a as zoom,b as zoomRect,c as zoomScale};
